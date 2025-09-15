541394d3376e4d97a616c1b0e365a3bb30b5f1b1
Chris <chrisladas98@gmail.com>
2025-09-16 01:03:52 +0300

    fix:

--- src/main/java/com/example/Meal_Planner/model/Meal.java
+++ src/main/java/com/example/Meal_Planner/model/Meal.java
@@ -4,17 +4,19 @@ package com.example.Meal_Planner.model;
 import com.example.Meal_Planner.core.enums.MealType;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 @Entity
-public class Meal {
+public class Meal extends AbstractEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,5 +31,19 @@ public class Meal {
     private String ingredients;
 
     private Integer prepTime;
+
+    public Meal(Object id, @NotNull(message = "Το όνομα δεν μπορεί να είναι null.")
+                           @Size(min = 2)
+                           String name,
+
+                           @NotNull(message = "Τα συστατικά δεν μπορούν να είναι null.")
+                           @Size(min = 2)
+                           String ingredients,
+
+                           String prepTime,
+
+                           @NotNull(message = "Ο τύπος δεν μπορεί να είναι null.")
+                           MealType mealType) {
+    }
 }
 
