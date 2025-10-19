25988fb744a8960b86a00438ec835fd0fed7a45a
Chris <chrisladas98@gmail.com>
2025-10-19 14:11:44 +0300

    Feat(MealReadOnlyDTO): remove MealReadOnlyDTO(), LocalDateTime(), change data type to prepTime

--- src/main/java/com/example/Meal_Planner/dto/MealReadOnlyDTO.java
+++ src/main/java/com/example/Meal_Planner/dto/MealReadOnlyDTO.java
@@ -1,30 +1,19 @@
 package com.example.Meal_Planner.dto;
 
 import com.example.Meal_Planner.core.enums.MealType;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class MealReadOnlyDTO {
-
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
     private String name;
     private String ingredients;
-    private String prepTime;
+    private Long prepTime;
     private Long id;
     private MealType mealType;
-
-    public MealReadOnlyDTO(LocalDateTime createdAt, LocalDateTime updatedAt,
-                           @NotBlank(message = "Η ονομασία είναι υποχρεωτική.") String name,
-                           String ingredients, Long id, MealType mealType, Integer prepTime) {
-    }
 }
