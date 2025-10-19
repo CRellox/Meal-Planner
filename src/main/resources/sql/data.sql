c655a0cd324d9ced394ce7c090c38b9894cfbaf4
Chris <chrisladas98@gmail.com>
2025-10-19 13:41:24 +0300

    chore: typo

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO meal (name, meal_type, ingredients, prep_time) VALUES
+INSERT INTO meals (name, meal_type, ingredients, prep_time) VALUES
 ('Ομελέτα', 'BREAKFAST', 'Αυγά, Γάλα, Ελαιόλαδο', 15),
 ('Σαλάτα Καίσαρα', 'LUNCH', 'Μαρούλι, Παρμεζάνα, Κρουτόν', 20),
 ('Ψητό Κοτόπουλο', 'DINNER', 'Κοτόπουλο, Ελαιόλαδο', 45);
