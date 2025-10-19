62be7ef192f1721aafc9ab7462b18312915ec577
Chris <chrisladas98@gmail.com>
2025-10-19 13:44:44 +0300

    fix: changing mapper

--- src/main/java/com/example/Meal_Planner/mapper/Mapper.java
+++ src/main/java/com/example/Meal_Planner/mapper/Mapper.java
@@ -10,16 +10,16 @@ import org.springframework.stereotype.Component;
 public class Mapper {
 
     public Meal mapToMealEntity(MealInsertDTO dto) {
-        return new Meal(null, dto.getName(), dto.getIngredients(), dto.getPrepTime(), dto.getMealType());
+        return new Meal(dto.getId(), dto.getName(), dto.getMealType(),dto.getIngredients(), dto.getPrepTime());
     }
 
     public MealReadOnlyDTO mapToMealReadOnlyDTO(Meal meal) {
-        return new MealReadOnlyDTO(meal.getCreatedAt(), meal.getUpdatedAt(), meal.getName(), meal.getIngredients(),
-                   meal.getId(), meal.getMealType(), meal.getPrepTime());
+        return new MealReadOnlyDTO(meal.getName(), meal.getIngredients(),
+                meal.getPrepTime(), meal.getId(), meal.getMealType());
     }
 
     public MealEditDTO mapToMealEditDTO(Meal meal) {
         return new MealEditDTO(meal.getName(), meal.getIngredients(),
-                               meal.getMealType(), meal.getPrepTime());
+                meal.getPrepTime(), meal.getMealType(), meal.getId());
     }
 }
