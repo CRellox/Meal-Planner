c6ae40a5266ed5c6b83bfd5082b9ace95447ca04
Chris <chrisladas98@gmail.com>
2025-10-19 14:07:59 +0300

    Feat(MealRepository): add @Repository, add findByIngredients()

--- src/main/java/com/example/Meal_Planner/repository/MealRepository.java
+++ src/main/java/com/example/Meal_Planner/repository/MealRepository.java
@@ -4,11 +4,13 @@ package com.example.Meal_Planner.repository;
 import com.example.Meal_Planner.core.enums.MealType;
 import com.example.Meal_Planner.model.Meal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface MealRepository extends JpaRepository<Meal, Long> {
 
     List<Meal> findByMealType(MealType mealType);
-    List<Meal> findByMealName(String name);
+    List<Meal> findByIngredients(String ingredients);
 }
