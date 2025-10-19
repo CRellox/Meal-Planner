1aef2a1ad3d0846fad45bdba6ec29828cc8a40ef
Chris <chrisladas98@gmail.com>
2025-10-19 18:33:22 +0300

    Feat: add exceptions logic

---
+++ src/main/java/com/example/Meal_Planner/core/exceptions/EntityAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.example.Meal_Planner.core.exceptions;
+
+public class EntityAlreadyExistsException extends EntityGenericException {
+    private static final String DEFAULT_CODE = "AlreadyExists";
+
+    public EntityAlreadyExistsException(String code, String message) {
+        super(code + DEFAULT_CODE, message);
+    }
+}
---
+++ src/main/java/com/example/Meal_Planner/core/exceptions/EntityGenericException.java
@@ -0,0 +1,13 @@
+package com.example.Meal_Planner.core.exceptions;
+
+import lombok.Getter;
+
+@Getter
+public class EntityGenericException extends Exception {
+    private final String code;
+
+    public EntityGenericException(String code, String message) {
+        super(message);
+        this.code = code;
+    }
+}
