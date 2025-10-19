3438d4d5943a0728789fe35f3c63a91430a2c1e6
Chris <chrisladas98@gmail.com>
2025-10-19 18:26:00 +0300

    Fix: remove findById()

--- src/main/java/com/example/Meal_Planner/repository/MealRepository.java
+++ src/main/java/com/example/Meal_Planner/repository/MealRepository.java
@@ -8,11 +8,9 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface MealRepository extends JpaRepository<Meal, Long>, JpaSpecificationExecutor<Meal> {
 
     List<Meal> findByMealType(MealType mealType);
-    Optional<Meal> findById(String id);
 }
