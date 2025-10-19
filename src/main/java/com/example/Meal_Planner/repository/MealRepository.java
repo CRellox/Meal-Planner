a18c82c6abcc5292984550ff80c4005ec091983b
Chris <chrisladas98@gmail.com>
2025-10-19 17:40:22 +0300

    Feat: add JpaSpecificationExecutor

--- src/main/java/com/example/Meal_Planner/repository/MealRepository.java
+++ src/main/java/com/example/Meal_Planner/repository/MealRepository.java
@@ -4,13 +4,13 @@ package com.example.Meal_Planner.repository;
 import com.example.Meal_Planner.core.enums.MealType;
 import com.example.Meal_Planner.model.Meal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface MealRepository extends JpaRepository<Meal, Long> {
+public interface MealRepository extends JpaRepository<Meal, Long>, JpaSpecificationExecutor<Meal> {
 
     List<Meal> findByMealType(MealType mealType);
-    List<Meal> findByIngredients(String ingredients);
 }
