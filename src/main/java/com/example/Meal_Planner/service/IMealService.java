2d71a6ca025b3abeaec6d1461e3adeeb6e29e72a
Chris <chrisladas98@gmail.com>
2025-10-19 17:26:24 +0300

    Feat: add IMealService

---
+++ src/main/java/com/example/Meal_Planner/service/IMealService.java
@@ -0,0 +1,14 @@
+package com.example.Meal_Planner.service;
+
+import com.example.Meal_Planner.dto.MealEditDTO;
+import com.example.Meal_Planner.dto.MealInsertDTO;
+import com.example.Meal_Planner.model.Meal;
+
+public interface IMealService {
+
+    Meal saveMeal(MealInsertDTO mealInsertDTO);
+
+    void updateMeal(MealEditDTO mealEditDTO);
+
+    void deleteMealById(String Id);
+}
