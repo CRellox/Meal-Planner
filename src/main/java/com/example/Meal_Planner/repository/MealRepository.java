908c5f09271975a10eab19d3996120ca728ba053
Chris <chrisladas98@gmail.com>
2025-10-19 17:43:20 +0300

    Feat: add findByMealById

--- src/main/java/com/example/Meal_Planner/repository/MealRepository.java
+++ src/main/java/com/example/Meal_Planner/repository/MealRepository.java
@@ -8,9 +8,11 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MealRepository extends JpaRepository<Meal, Long>, JpaSpecificationExecutor<Meal> {
 
     List<Meal> findByMealType(MealType mealType);
+    Optional<Meal> findByMealById(Long id);
 }
