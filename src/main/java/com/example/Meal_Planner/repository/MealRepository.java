dfe478f22b58c0b139b28dc00161d583cb165072
Chris <chrisladas98@gmail.com>
2025-11-04 10:48:35 +0200

    chore: remove unused imports

--- src/main/java/com/example/Meal_Planner/repository/MealRepository.java
+++ src/main/java/com/example/Meal_Planner/repository/MealRepository.java
@@ -1,13 +1,11 @@
 package com.example.Meal_Planner.repository;
 
 
-import com.example.Meal_Planner.core.enums.MealType;
 import com.example.Meal_Planner.model.Meal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface MealRepository extends JpaRepository<Meal, Long>, JpaSpecificationExecutor<Meal> {
