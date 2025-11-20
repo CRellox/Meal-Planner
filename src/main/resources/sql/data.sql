c1ed74d7f29c771c80ee96c45a3db4bf5d5c9694
Chris <chrisladas98@gmail.com>
2025-11-20 12:51:57 +0200

    chore: delete dummy data

--- src/main/resources/sql/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO meals (name, meal_type, ingredients, prep_time) VALUES
-('Ομελέτα', 'BREAKFAST', 'Αυγά, Γάλα, Ελαιόλαδο', 15),
-('Σαλάτα Καίσαρα', 'LUNCH', 'Μαρούλι, Παρμεζάνα, Κρουτόν', 20),
-('Ψητό Κοτόπουλο', 'DINNER', 'Κοτόπουλο, Ελαιόλαδο', 45);
