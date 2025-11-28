2e7637f1fea4f78f365781966efc4836aaa5bc60
Chris <chrisladas98@gmail.com>
2025-11-28 17:32:09 +0200

    feat(SecurityConfig): implement authentication and authorization. add password encoder for secure credential storage

--- src/main/java/com/example/Meal_Planner/authentication/SecurityConfig.java
+++ src/main/java/com/example/Meal_Planner/authentication/SecurityConfig.java
@@ -1,22 +1,55 @@
 package com.example.Meal_Planner.authentication;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final UserDetailsService userDetailsService;
+
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+
         http
-                .authorizeHttpRequests(authz -> authz
-                        .anyRequest().permitAll()
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/mealplanner/register",
+                                "/html/**" ,"/css/**", "/js/**", "/images/**").permitAll()
+                        .anyRequest().authenticated()
                 )
-                .csrf(csrf -> csrf.disable());  // Disable CSRF for development
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/mealplanner/meal/main-menu", false)
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login?logout")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                        .permitAll()
+                )
+                .userDetailsService(userDetailsService);
         return http.build();
     }
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(authz -> authz
+//                        .anyRequest().permitAll()
+//                )
+//                .csrf(csrf -> csrf.disable());  // Disable CSRF for development
+//        return http.build();
+//    }
 }
