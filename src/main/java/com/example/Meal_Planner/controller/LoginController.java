40fa066e8e8d5c82d263c3f4dd240ad0cce774ee
Chris <chrisladas98@gmail.com>
2025-11-28 17:23:52 +0200

    feat: add LoginController

---
+++ src/main/java/com/example/Meal_Planner/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.example.Meal_Planner.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(Principal principal) {
+        return principal == null ? "login-form" : "redirect:/mealplanner/meal/main-menu";
+    }
+
+    @GetMapping("/")
+    public String root(Principal principal) {
+        return principal == null ? "login-form" : "redirect:/mealplanner/meal/main-menu";
+    }
+}
